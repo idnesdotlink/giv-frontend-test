f65bb1bd4bc96ea8b12cd6241becf8a625305d73
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-08-20 05:42:36 +0700

    remove-json-viewer

--- src/main.js
+++ src/main.js
@@ -2,19 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import JsonViewer from 'vue-json-viewer'
 
 import './registerServiceWorker'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@mdi/font/css/materialdesignicons.min.css'
-import 'vue-json-viewer/style.css'
 
 import '@/assets/style.css'
 
 Vue.use(Buefy)
-Vue.use(JsonViewer)
 
 Vue.config.productionTip = false
 
